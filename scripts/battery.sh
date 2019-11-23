46495c832381d526ed5c56e37c1f5bb8f5a3c13b
Haseeb Ali <haseebali1@gmail.com>
2019-11-22 22:11:48 -0800

    changed format and added check to see if discharging and added battery percent to notify-send output

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-export XDG_RUNTIME_DIR="/run/user/1000"
+#export XDG_RUNTIME_DIR="/run/user/1000"
+
 #Code:
-pactl set-sink-volume 70%
-if [ `acpi -b | grep "Discharging" | awk ' { print ($4)-0}'`  -lt "15" ] ; then /usr/bin/mpg123 /home/haseeb/Music/Low-battery-sound.mp3  && notify-send "Battery Low"; fi
-if [ `acpi -b | grep 'Charging\|Not charging' | awk ' { print ($5)-0}'`  -eq "100" ] ; then /usr/bin/mpg123 /home/haseeb/Music/Low-battery-sound.mp3  && notify-send "Battery Full"; fi
-pactl set-sink-volume 25%
+
+bat_percent=$(acpi -b | grep "Discharging" | awk ' { print ($4)-0}')
+discharging=false;
+
+if [ "$bat_percent" -lt "15" ]  
+then 
+    /usr/bin/mpg123 -q /home/haseeb/Music/Low-battery-sound.mp3 && notify-send "Battery Low ${bat_percent}" 
+    discharging=true
+fi
+
+if [ "$discharging" = false ]
+then
+
+    bat_percent=$(acpi -b | grep 'Charging\|Not charging' | awk ' { print ($5)-0}')
+
+    if [ "$bat_percent" -eq "100" ]
+    then
+        /usr/bin/mpg123 /home/haseeb/Music/Low-battery-sound.mp3 && notify-send "Battery Full ${bat_percent}"
+    fi
+fi
+
