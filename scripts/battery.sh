5b9f9626752c5668eae28e56d2e92c1fb9fce2b2
Haseeb Ali <haseebali1@gmail.com>
2019-11-23 00:25:26 -0800

    fixed notify-send not working and removed the bool checking for discharging or not

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-#export XDG_RUNTIME_DIR="/run/user/1000"
+export XDG_RUNTIME_DIR="/run/user/1000"
+#export DISPLAY=:0.0
 
 #Code:
 
 bat_percent=$(acpi -b | grep "Discharging" | awk ' { print ($4)-0}')
-discharging=false;
 
 if [ "$bat_percent" -lt "15" ]  
 then 
     /usr/bin/mpg123 -q /home/haseeb/Music/Low-battery-sound.mp3 && notify-send "Battery Low ${bat_percent}" 
-    discharging=true
 fi
 
-if [ "$discharging" = false ]
+bat_percent=$(acpi -b | grep 'Charging\|Not charging' | awk ' { print ($NF)-0}')
+
+if [ "$bat_percent" -eq "100" ]
 then
-
-    bat_percent=$(acpi -b | grep 'Charging\|Not charging' | awk ' { print ($5)-0}')
-
-    if [ "$bat_percent" -eq "100" ]
-    then
-        /usr/bin/mpg123 /home/haseeb/Music/Low-battery-sound.mp3 && notify-send "Battery Full ${bat_percent}"
-    fi
+    /usr/bin/mpg123 -q /home/haseeb/Music/Low-battery-sound.mp3 && notify-send "Battery Full ${bat_percent}"
 fi
-
